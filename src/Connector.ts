28bf22450dc91a5a07d9ac5ea4f39b963446dcb8
Daniel Imhoff <dwieeb@gmail.com>
2023-02-04 18:30:05 -0600

    Re-license Peregrine for Web as MIT

--- src/Connector.ts
+++ src/Connector.ts
@@ -1,18 +1,3 @@
-// Peregrine for Web: native container for hybrid apps
-// Copyright (C) 2022 Caracal LLC
-//
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU General Public License version 3 as published
-// by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-// more details.
-//
-// You should have received a copy of the GNU General Public License version 3
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 import type RemoteFunctionRequest from './RemoteFunctionRequest'
 import type RemoteFunctionResponse from './RemoteFunctionResponse'
 
